f1822dbb5227ef2f9ca672b945494d6b4578ae61
Pandu-Jakkula <pandu.jakkula@bilvantis.io>
2025-03-02 11:42:56 +0530

    serve\\\\about///exp

--- app/page.js
+++ app/page.js
@@ -47,12 +47,12 @@ export default async function Home() {
     <div suppressHydrationWarning >
       <HeroSection />
       <AboutSection />
-      <Experience />
-      {/* <Skills />
+      {/* <Experience /> */}
+      <Skills />
       <Projects />
       <Education />
       <Service blogs={services} />
-      <ContactSection /> */}
+      <ContactSection />
     </div>
   )
 };
