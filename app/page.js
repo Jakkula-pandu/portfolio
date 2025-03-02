bc70ebc40083345d7d3faa43c30c80cd6832d323
Pandu-Jakkula <pandu.jakkula@bilvantis.io>
2025-03-02 11:46:50 +0530

    skills error

--- app/page.js
+++ app/page.js
@@ -48,7 +48,7 @@ export default async function Home() {
       <HeroSection />
       <AboutSection />
       {/* <Experience /> */} 
-      <Skills />
+      {/* <Skills /> */}
       <Projects />
       <Education />
       <Service blogs={services} />
