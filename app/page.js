fa2a40dce18aeec465ee153d4276fd1f56fe81b4
Pandu-Jakkula <pandu.jakkula@bilvantis.io>
2025-03-02 11:15:16 +0530

    nnnnnnnnn

--- app/page.js
+++ app/page.js
@@ -8,39 +8,39 @@ import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
 
-// async function getData() {
-//   const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
+async function getData() {
+  const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
 
-//   if (!res.ok) {
-//     throw new Error('Failed to fetch data')
-//   }
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
 
-//   const data = await res.json();
+  const data = await res.json();
 
-//   const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
+  const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
 
-//   return filtered;
-// };
+  return filtered;
+};
 
 export default async function Home() {
   // const blogs = await getData();
-  // const services = [
-  //   {
-  //     title: "Front-end Development",
-  //     description: "We specialize in creating responsive and interactive web applications using React.js, Angular, Material UI, Tailwind CSS, Bootstrap, and Redux. We focus on user-friendly interfaces and seamless user experiences.",
-  //     skills: ["React.js", "Angular", "Material UI", "Tailwind CSS", "Bootstrap", "Responsive Design", "Redux"]
-  //   },
-  //   {
-  //     title: "Full-stack Development",
-  //     description: "Our full-stack development covers both front-end and back-end technologies. We use Node.js for the back-end with Express for server-side logic, and integrate with front-end frameworks like React or Angular.",
-  //     skills: ["Node.js", "Express", "React.js", "Angular", "Full-stack"]
-  //   },
-  //   {
-  //     title: "Other Technologies",
-  //     description: "We work with a variety of technologies including MySQL, Python, and deployment strategies to ensure the best performance and scalability for your application.",
-  //     skills: ["MySQL", "Python", "Deployment", "APIs", "Backend"]
-  //   }
-  // ];
+  const services = [
+    {
+      title: "Front-end Development",
+      description: "We specialize in creating responsive and interactive web applications using React.js, Angular, Material UI, Tailwind CSS, Bootstrap, and Redux. We focus on user-friendly interfaces and seamless user experiences.",
+      skills: ["React.js", "Angular", "Material UI", "Tailwind CSS", "Bootstrap", "Responsive Design", "Redux"]
+    },
+    {
+      title: "Full-stack Development",
+      description: "Our full-stack development covers both front-end and back-end technologies. We use Node.js for the back-end with Express for server-side logic, and integrate with front-end frameworks like React or Angular.",
+      skills: ["Node.js", "Express", "React.js", "Angular", "Full-stack"]
+    },
+    {
+      title: "Other Technologies",
+      description: "We work with a variety of technologies including MySQL, Python, and deployment strategies to ensure the best performance and scalability for your application.",
+      skills: ["MySQL", "Python", "Deployment", "APIs", "Backend"]
+    }
+  ];
 
 
   return (
@@ -56,7 +56,8 @@ export default async function Home() {
      
     // </div>
     <div>
-      Hello World wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
+      Hello World
+      
     </div>
   )
 };
